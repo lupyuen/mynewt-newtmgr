194928c93c14778d4862204b0911de9ddcbc8070
lupyuen <luppy@appkaki.com>
2020-06-10 07:37:52 +0800

    Clean up

--- newtmgr.dart
+++ newtmgr.dart
@@ -10,48 +10,48 @@ const NMP_HDR_SIZE = 8;
 
 /// SMP Header
 class NmpHdr {
-	int Op;    //  uint8: 3 bits of opcode
-	int Flags; //  uint8
-	int Len;   //  uint16
-	int Group; //  uint16
-	int Seq;   //  uint8
-	int Id;    //  uint8
+  int Op;    //  uint8: 3 bits of opcode
+  int Flags; //  uint8
+  int Len;   //  uint16
+  int Group; //  uint16
+  int Seq;   //  uint8
+  int Id;    //  uint8
   
   /// Construct an SMP Header
   NmpHdr(
     this.Op,    //  uint8: 3 bits of opcode
-	  this.Flags, //  uint8
-	  this.Len,   //  uint16
-	  this.Group, //  uint16
-	  this.Seq,   //  uint8
-	  this.Id     //  uint8
+    this.Flags, //  uint8
+    this.Len,   //  uint16
+    this.Group, //  uint16
+    this.Seq,   //  uint8
+    this.Id     //  uint8
   );
   
   /// Return this SMP Header as a list of bytes
   typed.Uint8Buffer Bytes() {  //  Returns []byte
-	  var buf = typed.Uint8Buffer();  //  make([]byte, 0, NMP_HDR_SIZE);
+    var buf = typed.Uint8Buffer();  //  make([]byte, 0, NMP_HDR_SIZE);
     
     buf.add(this.Op);
-	  buf.add(this.Flags);
+    buf.add(this.Flags);
 
-	  typed.Uint8Buffer u16b = binaryBigEndianPutUint16(this.Len);
-	  buf.addAll(u16b);
+    typed.Uint8Buffer u16b = binaryBigEndianPutUint16(this.Len);
+    buf.addAll(u16b);
 
-	  u16b = binaryBigEndianPutUint16(this.Group);
-	  buf.addAll(u16b);
+    u16b = binaryBigEndianPutUint16(this.Group);
+    buf.addAll(u16b);
 
-	  buf.add(this.Seq);
-	  buf.add(this.Id);
+    buf.add(this.Seq);
+    buf.add(this.Id);
     assert(buf.length == NMP_HDR_SIZE);
 
-	  return buf;
+    return buf;
   }  
 }
 
 /// SMP Message
 class NmpMsg {
-	NmpHdr Hdr;
-	NmpReq Body;  //  Previously interface{}
+  NmpHdr Hdr;
+  NmpReq Body;  //  Previously interface{}
 
   /// Construct an SMP Message
   NmpMsg(this.Hdr, this.Body);
@@ -59,56 +59,56 @@ class NmpMsg {
 
 /// SMP Request Message
 abstract class NmpReq {
-	NmpHdr Hdr();
-	void SetHdr(NmpHdr hdr);
+  NmpHdr Hdr();
+  void SetHdr(NmpHdr hdr);
 
-	NmpMsg Msg();
+  NmpMsg Msg();
   void Encode(cbor.Encoder encoder);
 }
 
 /// SMP Response Message
 abstract class NmpRsp {
-	NmpHdr Hdr();
-	void SetHdr(NmpHdr msg);
+  NmpHdr Hdr();
+  void SetHdr(NmpHdr msg);
 
-	NmpMsg Msg();
+  NmpMsg Msg();
 }
 
 /// SMP Base Message
 mixin NmpBase {
-	NmpHdr hdr;  //  Will not be encoded: `codec:"-"`
+  NmpHdr hdr;  //  Will not be encoded: `codec:"-"`
   
   NmpHdr Hdr() {
-	  return hdr;
+    return hdr;
   }
   
   void SetHdr(NmpHdr h) {
-	  hdr = h;
+    hdr = h;
   }
 }
 
 NmpMsg MsgFromReq(NmpReq r) {
-	return NmpMsg(
-		r.Hdr(),
-		r
-	);
+  return NmpMsg(
+    r.Hdr(),
+    r
+  );
 }
 
 NmpMsg NewNmpMsg() {
-	return NmpMsg(
+  return NmpMsg(
     NmpHdr(0, 0, 0, 0, 0, 0),
     null
   );
 }
 
 NmpHdr DecodeNmpHdr(typed.Uint8Buffer data /* []byte */) {
-	if (data.length < NMP_HDR_SIZE) {
+  if (data.length < NMP_HDR_SIZE) {
     throw Exception(
       "Newtmgr request buffer too small ${data.length} bytes"
     );
-	}
+  }
 
-	final hdr = NmpHdr(
+  final hdr = NmpHdr(
     data[0],  //  Op:    uint8
     data[1],  //  Flags: uint8
     binaryBigEndianUint16(data[2], data[3]),  //  Len: binary.BigEndian.Uint16
@@ -117,7 +117,7 @@ NmpHdr DecodeNmpHdr(typed.Uint8Buffer data /* []byte */) {
     data[7],  //  Id:    uint8       
   );
 
-	return hdr;
+  return hdr;
 }
 
 /// Encode SMP Request Body with CBOR and return the byte array
@@ -144,23 +144,23 @@ typed.Uint8Buffer BodyBytes(
   // Finally to JSON
   print(inst.decodedToJSON());
 
-	print("Encoded ${ inst.decodedToJSON() } to:\n${ hexDump(data) }");
-	return data;
+  print("Encoded ${ inst.decodedToJSON() } to:\n${ hexDump(data) }");
+  return data;
 }
 
 /// Encode the SMP Message with CBOR and return the byte array
 typed.Uint8Buffer EncodeNmpPlain(NmpMsg nmr) {  //  Returns []byte
-	final bb = BodyBytes(nmr.Body);
+  final bb = BodyBytes(nmr.Body);
 
-	nmr.Hdr.Len = bb.length;  //  uint16
+  nmr.Hdr.Len = bb.length;  //  uint16
 
-	final hb = nmr.Hdr.Bytes();
+  final hb = nmr.Hdr.Bytes();
   var data = typed.Uint8Buffer();
   data.addAll(hb);
   data.addAll(bb);
 
-	print("Encoded:\n${ hexDump(data) }");
-	return data;
+  print("Encoded:\n${ hexDump(data) }");
+  return data;
 }
 
 /// Init the SMP Request and set the sequence number
@@ -171,16 +171,16 @@ void fillNmpReqWithSeq(
   int id,     //  uint8
   int seq     //  uint8
 ) {
-	final hdr = NmpHdr(
-		op,     //  Op
-		0,      //  Flags
-		0,      //  Len
-		group,  //  Group
-		seq,    //  Seq
-		id      //  Id
-	);
+  final hdr = NmpHdr(
+    op,     //  Op
+    0,      //  Flags
+    0,      //  Len
+    group,  //  Group
+    seq,    //  Seq
+    id      //  Id
+  );
 
-	req.SetHdr(hdr);
+  req.SetHdr(hdr);
 }
 
 /// Init the SMP Request and set the next sequence number
@@ -190,7 +190,7 @@ void fillNmpReq(
   int group,  //  uint16
   int id      //  uint8
 ) {
-	fillNmpReqWithSeq(
+  fillNmpReqWithSeq(
     req, 
     op, 
     group, 
@@ -230,7 +230,7 @@ String hexDump(typed.Uint8Buffer buf) {
 //////////////////////////////////////////////////////////////////////////////
 
 class ImageStateReadCmd {
-	CmdBase base;
+  CmdBase base;
 
   ImageStateReadCmd(this.base);
 
@@ -249,7 +249,7 @@ class ImageStateReadCmd {
 }
 
 class ImageStateReadResult {
-	ImageStateRsp Rsp;  //  Previously nmp.ImageStateRsp
+  ImageStateRsp Rsp;  //  Previously nmp.ImageStateRsp
 
   int Status() {
     return this.Rsp.Rc;
@@ -257,13 +257,13 @@ class ImageStateReadResult {
 }
 
 ImageStateReadCmd NewImageStateReadCmd() {
-	return ImageStateReadCmd(
-		NewCmdBase()
+  return ImageStateReadCmd(
+    NewCmdBase()
   );
 }
 
 ImageStateReadResult newImageStateReadResult() {
-	return ImageStateReadResult();
+  return ImageStateReadResult();
 }
 
 ////////////////////////////////////////
@@ -316,7 +316,7 @@ class ImageStateReadReq
   with NmpBase       //  Get and set SMP Message Header
   implements NmpReq  //  SMP Request Message  
 {
-	NmpBase base;  //  Will not be encoded: `codec:"-"`
+  NmpBase base;  //  Will not be encoded: `codec:"-"`
 
   NmpMsg Msg() { return MsgFromReq(this); }
 
@@ -345,9 +345,9 @@ class ImageStateReadReq
 */
 
 ImageStateReadReq NewImageStateReadReq() {
-	var r = ImageStateReadReq();
-	fillNmpReq(r, NMP_OP_READ, NMP_GROUP_IMAGE, NMP_ID_IMAGE_STATE);
-	return r;
+  var r = ImageStateReadReq();
+  fillNmpReq(r, NMP_OP_READ, NMP_GROUP_IMAGE, NMP_ID_IMAGE_STATE);
+  return r;
 }
 
 /* TODO
@@ -375,20 +375,20 @@ bool nmpSeqBeenRead = false;
 
 /// Return the next SMP Message Sequence Number, 0 to 255. The first number is random.
 int NextNmpSeq() {  //  Returns uint8
-	//  TODO: seqMutex.Lock()
-	//  TODO: defer seqMutex.Unlock()
+  //  TODO: seqMutex.Lock()
+  //  TODO: defer seqMutex.Unlock()
 
-	if (!nmpSeqBeenRead) {
+  if (!nmpSeqBeenRead) {
     //  First number is random
     var rng = new Random();
-		nextNmpSeq = rng.nextInt(256);  //  Returns 0 to 255
-		nmpSeqBeenRead = true;
-	}
+    nextNmpSeq = rng.nextInt(256);  //  Returns 0 to 255
+    nmpSeqBeenRead = true;
+  }
 
-	final val = nextNmpSeq;
-	nextNmpSeq = (nextNmpSeq + 1) % 256;
+  final val = nextNmpSeq;
+  nextNmpSeq = (nextNmpSeq + 1) % 256;
   assert(val >= 0 && val <= 255);
-	return val;
+  return val;
 }
 
 ////////////////////////////////////////
@@ -396,86 +396,86 @@ int NextNmpSeq() {  //  Returns uint8
 //  Converted from Go: https://github.com/lupyuen/mynewt-newtmgr/blob/master/nmxact/nmp/defs.go
 
 const
-	NMP_OP_READ      = 0,
-	NMP_OP_READ_RSP  = 1,
-	NMP_OP_WRITE     = 2,
-	NMP_OP_WRITE_RSP = 3;
+  NMP_OP_READ      = 0,
+  NMP_OP_READ_RSP  = 1,
+  NMP_OP_WRITE     = 2,
+  NMP_OP_WRITE_RSP = 3;
 
 const
-	NMP_ERR_OK       = 0,
-	NMP_ERR_EUNKNOWN = 1,
-	NMP_ERR_ENOMEM   = 2,
-	NMP_ERR_EINVAL   = 3,
-	NMP_ERR_ETIMEOUT = 4,
-	NMP_ERR_ENOENT   = 5;
+  NMP_ERR_OK       = 0,
+  NMP_ERR_EUNKNOWN = 1,
+  NMP_ERR_ENOMEM   = 2,
+  NMP_ERR_EINVAL   = 3,
+  NMP_ERR_ETIMEOUT = 4,
+  NMP_ERR_ENOENT   = 5;
 
 // First 64 groups are reserved for system level newtmgr commands.
 // Per-user commands are then defined after group 64.
 
 const
-	NMP_GROUP_DEFAULT = 0,
-	NMP_GROUP_IMAGE   = 1,
-	NMP_GROUP_STAT    = 2,
-	NMP_GROUP_CONFIG  = 3,
-	NMP_GROUP_LOG     = 4,
-	NMP_GROUP_CRASH   = 5,
-	NMP_GROUP_SPLIT   = 6,
-	NMP_GROUP_RUN     = 7,
-	NMP_GROUP_FS      = 8,
-	NMP_GROUP_SHELL   = 9,
-	NMP_GROUP_PERUSER = 64;
+  NMP_GROUP_DEFAULT = 0,
+  NMP_GROUP_IMAGE   = 1,
+  NMP_GROUP_STAT    = 2,
+  NMP_GROUP_CONFIG  = 3,
+  NMP_GROUP_LOG     = 4,
+  NMP_GROUP_CRASH   = 5,
+  NMP_GROUP_SPLIT   = 6,
+  NMP_GROUP_RUN     = 7,
+  NMP_GROUP_FS      = 8,
+  NMP_GROUP_SHELL   = 9,
+  NMP_GROUP_PERUSER = 64;
 
 // Default group (0).
 const
-	NMP_ID_DEF_ECHO           = 0,
-	NMP_ID_DEF_CONS_ECHO_CTRL = 1,
-	NMP_ID_DEF_TASKSTAT       = 2,
-	NMP_ID_DEF_MPSTAT         = 3,
-	NMP_ID_DEF_DATETIME_STR   = 4,
-	NMP_ID_DEF_RESET          = 5;
+  NMP_ID_DEF_ECHO           = 0,
+  NMP_ID_DEF_CONS_ECHO_CTRL = 1,
+  NMP_ID_DEF_TASKSTAT       = 2,
+  NMP_ID_DEF_MPSTAT         = 3,
+  NMP_ID_DEF_DATETIME_STR   = 4,
+  NMP_ID_DEF_RESET          = 5;
 
 // Image group (1).
 const
-	NMP_ID_IMAGE_STATE    = 0,
-	NMP_ID_IMAGE_UPLOAD   = 1,
-	NMP_ID_IMAGE_CORELIST = 3,
-	NMP_ID_IMAGE_CORELOAD = 4,
-	NMP_ID_IMAGE_ERASE    = 5;
+  NMP_ID_IMAGE_STATE    = 0,
+  NMP_ID_IMAGE_UPLOAD   = 1,
+  NMP_ID_IMAGE_CORELIST = 3,
+  NMP_ID_IMAGE_CORELOAD = 4,
+  NMP_ID_IMAGE_ERASE    = 5;
 
 // Stat group (2).
 const
-	NMP_ID_STAT_READ = 0,
-	NMP_ID_STAT_LIST = 1;
+  NMP_ID_STAT_READ = 0,
+  NMP_ID_STAT_LIST = 1;
 
 // Config group (3).
 const
-	NMP_ID_CONFIG_VAL = 0;
+  NMP_ID_CONFIG_VAL = 0;
 
 // Log group (4).
 const
-	NMP_ID_LOG_SHOW        = 0,
-	NMP_ID_LOG_CLEAR       = 1,
-	NMP_ID_LOG_APPEND      = 2,
-	NMP_ID_LOG_MODULE_LIST = 3,
-	NMP_ID_LOG_LEVEL_LIST  = 4,
-	NMP_ID_LOG_LIST        = 5;
+  NMP_ID_LOG_SHOW        = 0,
+  NMP_ID_LOG_CLEAR       = 1,
+  NMP_ID_LOG_APPEND      = 2,
+  NMP_ID_LOG_MODULE_LIST = 3,
+  NMP_ID_LOG_LEVEL_LIST  = 4,
+  NMP_ID_LOG_LIST        = 5;
 
 // Crash group (5).
 const
-	NMP_ID_CRASH_TRIGGER = 0;
+  NMP_ID_CRASH_TRIGGER = 0;
 
 // Run group (7).
 const
-	NMP_ID_RUN_TEST = 0,
-	NMP_ID_RUN_LIST = 1;
+  NMP_ID_RUN_TEST = 0,
+  NMP_ID_RUN_LIST = 1;
 
 // File system group (8).
 const
-	NMP_ID_FS_FILE = 0;
+  NMP_ID_FS_FILE = 0;
 
 // Shell group (8).
 const
-	NMP_ID_SHELL_EXEC = 0;
+  NMP_ID_SHELL_EXEC = 0;
 
 ////////////////////////////////////////
 
@@ -541,7 +541,7 @@ int main() {
 
   //  Get the encoded body
   final data = inst.output.getData();
-	print("Encoded ${ inst.decodedToJSON() } to:\n${ hexDump(data) }");
+  print("Encoded ${ inst.decodedToJSON() } to:\n${ hexDump(data) }");
 
   return 0;
 }
