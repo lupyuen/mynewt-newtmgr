79296fd937247cdee9978e24c4addbc5a533226d
lupyuen <luppy@appkaki.com>
2020-06-13 18:35:29 +0800

    Added decode test

--- newtmgr.dart
+++ newtmgr.dart
@@ -645,6 +645,8 @@ TxOptions NewTxOptions() {
 
 void main() {
   composeRequest();
+  //  testCborEncoding();
+  //  testCborDecoding();
 }
 
 /// Compose a request to query firmware images on PineTime
@@ -677,8 +679,8 @@ void testCommand() {
   //  imageStatePrintRsp(ires.Rsp);
 }
 
-/// Test the CBOR library
-void testCbor() {
+/// Test the CBOR library for Encoding. Based on https://github.com/shamblett/cbor/blob/master/example/cbor_map_builder.dart
+void testCborEncoding() {
   /// An example of using the Map Builder class.
   /// Map builder is used to build maps with complex values such as tag values, indefinite sequences
   /// and the output of other list or map builders.
@@ -736,4 +738,43 @@ void testCbor() {
   //  Get the encoded body
   final data = inst.output.getData();
   print("Encoded ${ inst.decodedToJSON() } to:\n${ hexDump(data) }");
+}
+
+/// Test the CBOR library for Decoding. Based on https://github.com/shamblett/cbor/blob/master/example/cbor_payload_decode.dart
+void testCborDecoding() {
+  /// An example of using the Map Builder class.
+  /// Map builder is used to build maps with complex values such as tag values, indefinite sequences
+  /// and the output of other list or map builders.
+
+  final payload = <int>[
+    0xbf, 0x66, 0x69, 0x6d, 0x61, 0x67, 0x65, 0x73, // |.fimages|
+    0x9f, 0xbf, 0x64, 0x73, 0x6c, 0x6f, 0x74, 0x00, 0x67, 0x76, 0x65, 0x72, 0x73, 0x69, 0x6f, 0x6e, // |..dslot.gversion|
+    0x65, 0x31, 0x2e, 0x30, 0x2e, 0x30, 0x64, 0x68, 0x61, 0x73, 0x68, 0x58, 0x20, 0x70, 0x3e, 0xbb, // |e1.0.0dhashX, 0xp>.|
+    0xf8, 0x11, 0x45, 0x8b, 0x1f, 0xad, 0x18, 0x9e, 0x64, 0xe3, 0xa5, 0xe0, 0xf8, 0x09, 0xcb, 0xe6, // |..E.....d.......|
+    0xba, 0xd8, 0x83, 0xc7, 0x6b, 0x3d, 0xd7, 0x12, 0x79, 0x1c, 0x82, 0x2f, 0xb5, 0x68, 0x62, 0x6f, // |....k=..y../.hbo|
+    0x6f, 0x74, 0x61, 0x62, 0x6c, 0x65, 0xf5, 0x67, 0x70, 0x65, 0x6e, 0x64, 0x69, 0x6e, 0x67, 0xf4, // |otable.gpending.|
+    0x69, 0x63, 0x6f, 0x6e, 0x66, 0x69, 0x72, 0x6d, 0x65, 0x64, 0xf5, 0x66, 0x61, 0x63, 0x74, 0x69, // |iconfirmed.facti|
+    0x76, 0x65, 0xf5, 0x69, 0x70, 0x65, 0x72, 0x6d, 0x61, 0x6e, 0x65, 0x6e, 0x74, 0xf4, 0xff, 0xff, // |ve.ipermanent...|
+    0x6b, 0x73, 0x70, 0x6c, 0x69, 0x74, 0x53, 0x74, 0x61, 0x74, 0x75, 0x73, 0x00, 0xff              // |ksplitStatus..| 
+  ];
+
+  // Get our cbor instance, always do this,it correctly
+  // initialises the decoder.
+  final inst = cbor.Cbor();
+
+  final payloadBuffer = typed.Uint8Buffer();
+  payloadBuffer.addAll(payload);
+
+  // Decode from the buffer, you can also decode from the
+  // int list if you prefer.
+  inst.decodeFromBuffer(payloadBuffer);
+
+  // Pretty print, note that these methods use [GetDecodedData] and will
+  // thus build the payload buffer.
+  // If you do not want to pretty print or use Json just get the list of
+  // decoded data directly by calling [GetDecodedData()]
+  print(inst.decodedPrettyPrint());
+
+  // JSON, maps can only have string keys to decode to JSON
+  print(inst.decodedToJSON());
 }
