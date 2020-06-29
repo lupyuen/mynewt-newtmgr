2267f0f42fd2872f7e7b0419f67319fc2c8745c9
lupyuen <luppy@appkaki.com>
2020-06-29 09:05:44 +0800

    Added logs

--- scripts/list-firmware.sh
+++ scripts/list-firmware.sh
@@ -1,7 +1,9 @@
-# List Firmware
+# List Firmware on PineTime
 set -e -x
 
 cd newtmgr
 sudo ./newtmgr conn add pinetime type=ble connstring="peer_name=pinetime"
 
-sudo ./newtmgr image list -c pinetime --loglevel debug
+sudo ./newtmgr image list -c pinetime --loglevel debug \
+    >../logs/list-firmware.log 2>&1
+cat ../logs/list-firmware.log
--- scripts/upload-firmware.sh
+++ scripts/upload-firmware.sh
@@ -1,6 +1,8 @@
-# Upload Firmware
+# Upload Firmware to PineTime
 set -e -x
 
 cd newtmgr
-sudo ./newtmgr image upload -c pinetime \
-   ~/my_sensor_app_1.1.img
+sudo ./newtmgr image upload -c pinetime --loglevel debug \
+   ~/my_sensor_app_1.1.img \
+    >../logs/upload-firmware.log 2>&1
+cat ../logs/upload-firmware.log
